84d618b76065bcedd9529713b7ace358460066ba
HAjulien <96483338+HAjulien@users.noreply.github.com>
2022-04-22 12:36:26 +0200

    creation backOffice des categories back

--- src/Controller/Admin/CategoryController.php
+++ src/Controller/Admin/CategoryController.php
@@ -3,18 +3,52 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Category;
+use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[Route('/admin/category', name: 'admin_category_')]
 class CategoryController extends AbstractController
 {
-    #[Route('/category/{slug}', name: 'app_category')]
-    public function index(Category $category): Response
+    #[Route('/', name: 'index')]
+    public function index(CategoryRepository $categoryRepository): Response
     {
-        //dd($category->getPosts());
-        return $this->render('category/index.html.twig', [
-            'category' => $category,
+                $category = $categoryRepository->findAll();
+
+        return $this->render('admin/category/index.html.twig', [
+            'categories' => $category,
         ]);
     }
+
+    #[Route("/add", name:"add")]
+    public function addCategory(Request $request, ManagerRegistry $doctrine): Response
+    {
+        // dd($request);
+        $category = new Category();
+        // dd($category);
+        $form = $this->createForm(CategoryType::class, $category);
+        // dd($form);
+        // dd($form->createView());
+
+        
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $em = $this->getDoctrine()->getManager(); ancienne depreciée
+            $em = $doctrine->getManager();
+            $em->persist($category);
+            $em->flush();
+            return $this->redirectToRoute('admin_home');
+        }
+        
+        
+        return $this->render('admin/category/add.html.twig', [
+            'controller_name' => 'AdminAddCategory',
+            'form' => $form->createView(),
+        ]);
+    }
+    
 }
